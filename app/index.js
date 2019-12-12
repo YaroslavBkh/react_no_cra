bfd5de5c5805492e830de83a5b08751541ae00e0
CodeAgony <yaroslav.bkh@gmail.com>
2019-12-12 13:36:16 +0200

    Use linting

--- app/index.js
+++ app/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>Hello World</div>
-        )
-    }
-}
+const App = () => {
+  return <div>Hello World</div>;
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
